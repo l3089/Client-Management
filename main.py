a9acf10ea5bd264473e8a946157f5f1c12532cec
Francisco <alemanorellana@gmail.com>
2022-01-27 12:46:12 -0600

    Create a login template rendering the form with WTF

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template,session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app) #Initiate bootstrap (clase 15 del curso)
@@ -9,7 +11,11 @@ bootstrap = Bootstrap(app) #Initiate bootstrap (clase 15 del curso)
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 
-
+#Create fields and instances for validating purpouses
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 #When a route is not found we modify the default 404 error
 @app.errorhandler(404)
@@ -36,6 +42,20 @@ def add():
 @app.route('/test')
 def test():
     return render_template('bstemplate.html')
+
+@app.route('/login')
+def login():
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+
+    #Create a context to pass all vars in it and avoid to pass each var separately
+    context = {
+        'user_ip': user_ip,
+        'login_form': login_form
+    }
+    return render_template('login.html', **context)
+
+
     
 if __name__ == '__main__':
    app.run()
