a2c739774e49f7a8c33da8aab3dfae3d790f2632
Francisco <alemanorellana@gmail.com>
2022-01-27 11:52:20 -0600

    Cambios en form add-client

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 
 FLASK_DEBUG=1
+FLASK_ENV=development
+FLASK_APP=main.py
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template,session
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app) #Initiate bootstrap (clase 15 del curso)
+#clase 15, ordenar logo y navbar
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+
+
 
 #When a route is not found we modify the default 404 error
 @app.errorhandler(404)
@@ -11,12 +20,13 @@ def not_found(error):
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session ['user_ip']= user_ip
+    #response.set_cookie('user_ip', user_ip)
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     return render_template('hello.html', user_ip=user_ip)
 
 @app.route('/registro-clientes')
